169883ff7e43e4955ad360d29e3953dd4e261e30
bryan <ayenwaluyan@gmail.com>
2026-06-17 10:43:39 +0800

    revisi 24

--- app/Http/Controllers/Api/MaterialController.php
+++ app/Http/Controllers/Api/MaterialController.php
@@ -9,7 +9,21 @@ class MaterialController extends Controller
 {
     public function show($id)
     {
-        $material = Material::with('topic')->findOrFail($id);
-        return response()->json($material);
+        $material = Material::with('topic:id,title')->findOrFail($id);
+    
+        return response()->json([
+            'id'               => $material->id,
+            'topic_id'         => $material->topic_id,
+            'title'            => $material->title,
+            'content'          => $material->content,
+            'video_url'        => $material->video_url,
+            'duration_minutes' => $material->duration_minutes,
+            'order'            => $material->order,
+            'topic'            => $material->topic,
+            'file_name'        => $material->file_name,
+            'file_url'         => $material->file_path
+                                    ? url('/api/files/' . $material->file_path)
+                                    : null,
+        ]);
     }
 }
